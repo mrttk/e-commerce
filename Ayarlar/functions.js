14b287a2b06fb53cde15997d212524ccc89fa7df
emre-tetik <blackpearl_39@hotmail.com>
2019-07-07 03:25:40 +0300

    Ürün resim geçişleri için js kodu yazıldı.

--- Ayarlar/functions.js
+++ Ayarlar/functions.js
@@ -1,7 +1,12 @@
-$(document).ready(function () {
-    $.SoruIcerigiGoster = function (ElemanIDsi) {
+$(document).ready(function() {
+    $.SoruIcerigiGoster = function(ElemanIDsi) {
         var IslenecekAlan = "#" + ElemanIDsi;
         $(".SorununCevapAlani").slideUp();
         $(IslenecekAlan).parent().find(".SorununCevapAlani").slideToggle();
-    }
+    };
+
+    $.UrunDetayResmiDegistir = function(klasor, resimDegeri) {
+        var resimIcinDosyaYolu = "Resimler/UrunResimleri/" + klasor + "/" + resimDegeri;
+        $("#buyukresim").attr("src", resimIcinDosyaYolu);
+    };
 });
